a7349e64ac73d66c17619a4309f26b9b4983a12a
Philip Sinnott <philipsinnotts@gmail.com>
2021-12-02 13:51:17 +0100

    Day 1 part 2 complete

---
+++ day_1/d1_p2.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+
+import requests
+
+input = open("input.txt")
+
+input = input.read()
+input = input.splitlines()
+integer_map = map(int, input)
+input = list(integer_map)
+
+# Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+counter = 0
+# loop through list of values, set start pos to the first index of the "second sliding window"
+for i in range(3, len(input)):
+    if input[i] > input[i - 3]:
+        counter += 1
+print(counter)
+
+# for i in range(3, len(input)):
+#     print(input[i])
+#     print(input[i - 3])
+#
+#     if input[i] > input[i - 3]:
+#         print(True)
+#         # 6 true i rad, sedan 2 falska
+#     else:
+#         print(False)
